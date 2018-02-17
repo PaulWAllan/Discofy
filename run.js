cf04a8374c73fd0c11b8196b91df5987cb9ca419
Paul Laptop <allanpaulw@gmail.com>
2018-02-17 13:51:32 -0800

    Adding the command list and a few changes to run.js.

--- run.js
+++ run.js
@@ -1,16 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const token = config.token;
+const prefix = config.prefix;
+const commandList = require("./commandList.json");
 
 client.on("ready", () => {
     console.log("Discofy: Ready to roll out!");
 });
 
 client.on("message", (message) => {
-    if (message.content.startsWith(token + "play")) {
-        message.channel.send("pong!");//TODO: do play stuff here
-    } 
+    if (message.content.startsWith(prefix + "play")) {
+        message.channel.send("Make me do stuff!");//TODO: do play stuff here
+    } else if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("pong!");
+    } //else if (message.content.startsWith(prefix + "help")) {
+        
 });
 
-client.login(config.key);
+client.login(config.token);
