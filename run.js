0883c72f0b67ecd27fd92ab9f840705c15af05ee
Jason Wang <jason_wang94@hotmail.com>
2018-02-17 22:34:02 -0800

    queueing functionality

--- run.js
+++ run.js
@@ -1,24 +1,67 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const config = require("./config.json");
-const prefix = config.prefix;
 const commandList = require("./commandList.json");
-
-// Require the Spotify Web Helper
+const request = require("request-promise-native")
 const NodeSpotify = require ("node-spotify-helper");
+const Spotify = require('node-spotify-api');
+const Pauseable = require('pauseable');
+
+const client = new Discord.Client();
 const webApi = new NodeSpotify.SpotifyWebApi ();
 const webHelper = new NodeSpotify.SpotifyWebHelper ();
-var tracks = "";
+
+var spotify = new Spotify({
+  id: config.spotifyClientId,
+  secret: config.spotifyClientSecret
+});
+var trackQueue = [];
+
 
 async function init () {
-  // Connect to the Web Api using your clientId and clientSecret
-  await webApi.authenticate ("05e2cb49f24d40239bf468115cac64aa", "60cfc9d6da2c4a7b832a0d56e20e1498");
-  await webHelper.connect ();
+  await webHelper.connect();
 }
 
-async function playTrackFromQuery(query) {
-  tracks = await webApi.searchTracks (query);
-  await webHelper.play(tracks[0].uri);
+
+async function play() {
+  await webHelper.play(trackQueue[0].trackUrl);
+  //TODO: set timer for length of song - pause execution for length of song
+  var timeout = Pauseable.setTimeout(() => {
+    timeout.pause();
+    trackQueue.shift();
+    // console.log(trackQueue[0].trackName);
+    if (trackQueue.length > 0) {
+      play();
+    } else
+    if (trackQueue.length == 0) {
+      setTimeout(() => {
+        timeout.pause();
+        pauseTrack();
+      }, 1000);
+    }
+  }, trackQueue[0].trackDuration - 1000);
+}
+
+async function addTrackToQueue(queryT) {
+
+  await spotify
+    .search({ type: 'track', query: queryT })
+    .then(function(response) {
+      trackQueue.push({
+        trackName : response.tracks.items[0].name,
+        trackArtist: response.tracks.items[0].artists[0].name,
+        trackAlbum: response.tracks.items[0].album.name,
+        trackUrl : response.tracks.items[0].uri,
+        trackDuration : response.tracks.items[0].duration_ms
+      });
+      console.log(trackQueue[0]);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+
+    if (trackQueue.length == 1) {
+      play();
+    }
 }
 
 async function pauseTrack() {
@@ -35,20 +78,20 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
     if (command == "play") {
       // Run a query
       const [...query] = args.splice(0);
-      playTrackFromQuery(query.join(" "));
+      addTrackToQueue(query.join(" "));
     } else
     if (command == "pause") {
       pauseTrack();
     } else
     if (command == "unpause") {
       unpauseTrack();
-    } else 
+    } else
     if (command == "help") {
       var commands = '**Command List**' + '\n\n';
       for(var i in commandList){
